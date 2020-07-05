81a82b90da5a01da3ad4b2bc38fef1c16dd968b4
tyler <tyler-zy@163.com>
2020-07-05 09:31:34 +0800

    git-springBoot

--- src/main/java/com/atguigu/springboot/GitHotFix.java
+++ src/main/java/com/atguigu/springboot/GitHotFix.java
@@ -2,9 +2,7 @@ package com.atguigu.springboot;
 
 public class GitHotFix {
     public static void main(String[] args) {
-        System.out.println("githotfix111111BBB");
 
-
-        System.out.println("hotfix 在 8行添加");
+        System.out.println("hotfix 提交");
     }
 }
